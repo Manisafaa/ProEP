4e21c14870cbbca3c29c92387b243f5975e7074c
Mani Hamidisafa <mani.safaa@gmail.com>
2015-06-07 13:02:26 +0200

    2nd Increment
    Server is now hosted out of VS.
    old client service reference can be configured to use  the new server
    solution

---
+++ Second Increment/Service/ChatService/IChat.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ChatService
+{
+    [ServiceContract(Namespace = "Server", CallbackContract = typeof(IChatCallback))]
+    public interface IChat
+    {
+        [OperationContract]
+        //Kyrill: added Guid id to Subscribe so that the client can create its own id
+        User Subscribe(Guid id, string username);
+
+        [OperationContract]
+        List<ChatMessage> GetLastMessages();
+
+        [OperationContract(IsOneWay = true)]
+        void SendPublicMessage(Guid from, string message);
+
+        [OperationContract]
+        User ConnectWithUser(Guid from, Guid to);
+
+        [OperationContract(IsOneWay = true)]
+        void SendPrivateMessage(Guid from, string message, Guid to);
+
+        [OperationContract(IsOneWay = true)]
+        void Unsubscribe(Guid id);
+    }
+
+    [ServiceContract(Namespace = "Server", CallbackContract = typeof(IChatCallback))]
+    public interface IChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void BroadcastMessage(ChatMessage message);
+
+        [OperationContract(IsOneWay = true)]
+        void DeliverMessage(ChatMessage message);
+
+        [OperationContract]
+        bool OpenHost(User from);
+    }
+
+
+    [DataContract]
+    public class ChatMessage
+    {
+        [DataMember]
+        public User user { get; set; }
+
+        [DataMember]
+        public string text { get; set; }
+    }
+
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        //Kyrill: int id replaced by Guid id
+        public Guid id { get; set; }
+
+        [DataMember]
+        public string username { get; set; }
+
+        [DataMember]
+        public IChatCallback callback { get; set; }
+    }
+}
